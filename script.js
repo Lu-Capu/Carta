84d0de1b6d798758b71d516cd1b4806ec3f45a37
Lu-Capu <sebastianvilchez44@gmail.com>
2026-08-20 10:11:01 -0500

    ns

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function cambiarHoja(idHojaTarget) {
         // Ocultar todas las cartas
         document.querySelectorAll('.carta').forEach(carta => {
@@ -13,20 +12,4 @@ function cambiarHoja(idHojaTarget) {
         hojaTarget.style.animation = 'none';
         hojaTarget.offsetHeight; /* trigger reflow */
         hojaTarget.style.animation = null;
-=======
-function cambiarHoja(idHojaTarget) {
-        // Ocultar todas las cartas
-        document.querySelectorAll('.carta').forEach(carta => {
-            carta.classList.add('oculta');
-        });
-        
-        // Mostrar la hoja seleccionada
-        const hojaTarget = document.getElementById(idHojaTarget);
-        hojaTarget.classList.remove('oculta');
-
-        // Reiniciar la animación CSS
-        hojaTarget.style.animation = 'none';
-        hojaTarget.offsetHeight; /* trigger reflow */
-        hojaTarget.style.animation = null;
->>>>>>> 0234489a2c8a714bb21b6c3730d195b0171b4e1b
     }
